d7c8d565739ea48e047fded1a610730cd6255fd8
ayunami2000 <spwilliamsiam@gmail.com>
2023-03-22 15:09:46 -0400

    Saving, next & prev episode btns

--- web/script.js
+++ web/script.js
@@ -20,10 +20,74 @@ const nextEpisodeBtn = document.getElementById("nextepisode");
 const clearSearchBtn = document.getElementById("clearsearch");
 const downMediaBtn = document.getElementById("downmedia");
 const endpointCards = document.getElementById("endpointcards");
+const agreeBtn = document.getElementById("agree");
+const prevMediaBtn = document.getElementById("prevmedia");
+const nextMediaBtn = document.getElementById("nextmedia");
 
 const useHls = !media.canPlayType("application/vnd.apple.mpegurl") && Hls.isSupported();
 
 let hls = null;
+
+function loadPage(epOffset) {
+	if (window.location.search.length > 1) {
+		const params = new URLSearchParams(window.location.search.slice(1));
+		if (!params.has("gd") || !params.has("dl") || !params.has("q")) {
+			return false;
+		}
+		let vidSrc = "/";
+		const gd = !!+params.get("gd");
+		if (gd) {
+			vidSrc += "gd";
+		}
+		const dl = !!+params.get("dl");
+		const hasE = (gd && dl) ? params.has("episode") : params.has("e");
+		const e = (gd && dl) ? params.get("episode") : params.get("e");
+		if (dl) {
+			vidSrc += "dl/" + params.get("q");
+			if (hasE) {
+				const newEp = (+e) + epOffset;
+				if (newEp > 0) {
+					if (vidSrc.includes("?")) {
+						vidSrc += "&";
+					} else {
+						vidSrc += "?";
+					}
+					vidSrc += "e" + ((gd && dl) ? "pisode" : "") + "=" + encodeURIComponent(newEp);
+				}
+			} else if (epOffset > 0) {
+				if (vidSrc.includes("?")) {
+					vidSrc += "&";
+				} else {
+					vidSrc += "?";
+				}
+				vidSrc += "e" + ((gd && dl) ? "pisode" : "") + "=1";
+			}
+		} else {
+			if (!params.has("s")) {
+				return false;
+			}
+			if (gd && !params.has("t")) {
+				return false;
+			}
+			vidSrc += "sdl?s=" + encodeURIComponent(params.get("s"));
+			if (gd) {
+				vidSrc += "&t=" + encodeURIComponent(params.get("t"));
+			}
+			vidSrc += "&q=" + encodeURIComponent(params.get("q"));
+			if (hasE) {
+				const newEp = (+e) + epOffset;
+				if (newEp > 0) {
+					vidSrc += "&e" + ((gd && dl) ? "pisode" : "") + "=" + encodeURIComponent(newEp);
+				}
+			} else if (epOffset > 0) {
+				vidSrc += "&e" + ((gd && dl) ? "pisode" : "") + "=1";
+			}
+		}
+		loadVidUrl(vidSrc);
+		return true;
+	}
+	return false;
+}
 
 function addEndpointCard(eu, et) {
 	const card = document.createElement("div");
@@ -68,6 +132,14 @@ episodeBox.oninput = function() {
 	}
 };
 
+prevMediaBtn.onclick = function() {
+	loadPage(-1);
+};
+
+nextMediaBtn.onclick = function() {
+	loadPage(1);
+};
+
 prevEpisodeBtn.onclick = function() {
 	if (!isNaN(episodeBox.value) && +episodeBox.value > 1) {
 		episodeBox.value--;
@@ -96,6 +168,7 @@ closeMediaBtn.onclick = function() {
 		media.removeAttribute("src");
 	}
 	media.src = backup.src = "about:blank";
+	window.history.pushState({}, "", "/");
 };
 
 searchForm.onsubmit = function() {
@@ -108,8 +181,37 @@ searchForm.onsubmit = function() {
 	return false;
 };
 
+function savePage(vidSrc) {
+	const gd = vidSrc.startsWith("/gd");
+	const dl = vidSrc.startsWith("/dl/") || vidSrc.startsWith("/gddl/");
+	const qInd = vidSrc.indexOf("?");
+	let ur = "?gd=";
+	ur += gd ? 1 : 0;
+	ur += "&dl=";
+	ur += dl ? 1 : 0;
+	if (dl) {
+		ur += "&q=";
+		let eInd = vidSrc.indexOf("e" + ((gd && dl) ? "pisode" : "") + "=");
+		if (eInd != -1) {
+			if (qInd == eInd - 1) {
+				eInd -= 1;
+			} else if (vidSrc.indexOf("&e" + ((gd && dl) ? "pisode" : "") + "=") == eInd - 1) {
+				eInd -= 1;
+			} else {
+				eInd = -1;
+			}
+		}
+		ur += encodeURIComponent(vidSrc.slice(vidSrc.indexOf("/", 1) + 1, eInd == -1 ? undefined : eInd));
+		ur += eInd == -1 ? "" : "&" + vidSrc.slice(eInd + 1);
+	} else {
+		ur += qInd == -1 ? "" : "&" + vidSrc.slice(qInd + 1);
+	}
+	window.history.pushState({}, "", ur);
+}
+
 function loadVidUrl(vidSrc) {
 	closeMediaBtn.onclick();
+	savePage(vidSrc);
 	backup.classList.add("d-none");
 	media.classList.remove("d-none");
 	if(useHls) {
@@ -180,4 +282,12 @@ searchBox.oninput = function() {
 			searchSuggestBox.innerHTML = "";
 		});
 	}, 500);
+};
+
+agreeBtn.onclick = function(e) {
+	agreeBtn.onclick = function() {};
+	if (loadPage(0)) {
+		e.preventDefault();
+		window.location.href = "#video";
+	}
 };
